704f31aa61ec007e95fc8896dc5430226e92d386
ojeffpinheiro <jefersonpinheirodesouza@gmail.com>
2021-02-14 19:45:44 -0300

    animação modal

--- scripts.js
+++ scripts.js
@@ -228,6 +228,34 @@ function DarkMode() {
   darkSelect.addEventListener('change', onChangeDark)
 }
 
+function LoadingAppAnimation(){
+  const timeline = new TimelineMax({delay:1, onComplete:nextAnimation});
+  timeline
+    .fromTo(
+        '.bg-loader', 1 ,
+        { width:'100%' }, 
+        { width:'0%',delay: 5,ease:Expo.easeInOut } 
+    )
+    .fromTo(
+        'header', 2 ,
+        { width:'0%',opacity: 0 }, 
+        { width:'100%',opacity: 1 ,ease:Expo.easeInOut},
+        '-=1' 
+    )
+    .fromTo(
+        'main.container', 0.7 ,
+        { y:-50,opacity: 0 }, 
+        { y:0,opacity: 1 ,ease:Expo.easeInOut},
+        '-=0.5' 
+    )
+    .fromTo(
+      'footer p', 0.7 ,
+      { y:-50,opacity: 0 }, 
+      { y:0,opacity: 1 ,ease:Expo.easeInOut},
+      '-=0.5' 
+    )
+}
+
 const App = {
   init(){
     DarkMode()
